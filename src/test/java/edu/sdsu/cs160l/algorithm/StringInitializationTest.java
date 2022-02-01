5d754a1a4c7d6bcbb8642d309d0b1ccb06fd174a
HRSHDG8 <hrshdg8@gmail.com>
2022-02-01 09:18:17 -0800

    init

---
+++ src/test/java/edu/sdsu/cs160l/algorithm/StringInitializationTest.java
@@ -0,0 +1,25 @@
+package edu.sdsu.cs160l.algorithm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
+public class StringInitializationTest {
+
+    @Test
+    public void create2StringWithSameValue(){
+        String a = "hey";
+        String b = "hey";
+
+        //TODO write a suitable test to check equality of these strings
+    }
+
+    @Test
+    public void create2StringArrays(){
+        String[] a = {"java", "c++","javascript"};
+        String[] b = {"java", "c++","javascript"};
+
+        //TODO write a suitable test to check equality of these string arrays
+
+    }
+}
