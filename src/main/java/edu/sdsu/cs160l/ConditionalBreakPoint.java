05e3ef2c78947db5e4b363f6770a603d2e1747bf
HRSHDG8 <hrshdg8@gmail.com>
2022-02-07 20:08:34 -0800

    lab 4

---
+++ src/main/java/edu/sdsu/cs160l/ConditionalBreakPoint.java
@@ -0,0 +1,13 @@
+package edu.sdsu.cs160l;
+
+public class ConditionalBreakPoint {
+    public void breakOnConditionOnly(){
+        String[] data1 = {"mz", "my", "my", "mx", "mz", "mx", "my", "mz", "mz", "my", "my", "mx", "mz", "mx", "my", "mz"};
+
+        for(int i=0;i<data1.length;i++){
+            String s = "Super : "+data1[i];
+
+            System.out.println(s);
+        }
+    }
+}
---
+++ src/main/java/edu/sdsu/cs160l/ExceptionBreakPoint.java
@@ -0,0 +1,10 @@
+package edu.sdsu.cs160l;
+
+public class ExceptionBreakPoint {
+
+    public void breakOnException(){
+        String[] a = {"mz" , "mx", "mz","my","my"};
+        System.out.println(a[1]);
+        System.out.println(a[6]);
+    }
+}
