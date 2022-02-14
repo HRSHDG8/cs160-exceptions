19f51ba1653228540de62f9eac4d380043008fc7
HRSHDG8 <hrshdg8@gmail.com>
2022-02-14 15:14:58 -0800

    init

--- src/main/java/edu/sdsu/cs160l/Main.java
+++
@@ -1,8 +0,0 @@
-package edu.sdsu.cs160l;
-
-/**
- * Nothing much here, grab screenshot for all breakpoints you learn today
- * and your report consists for the majority of your score
- */
-public class Main {
-}
