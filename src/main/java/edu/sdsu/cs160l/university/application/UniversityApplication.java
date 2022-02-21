7f9ffc2cb8d4c548e3f20613e365b5439a0b2ec9
HRSHDG8 <hrshdg8@gmail.com>
2022-02-21 14:31:49 -0800

    Singleton

--- src/main/java/edu/sdsu/cs160l/university/application/UniversityApplication.java
+++ src/main/java/edu/sdsu/cs160l/university/application/UniversityApplication.java
@@ -1,10 +1,9 @@
 package edu.sdsu.cs160l.university.application;
 
-import edu.sdsu.cs160l.university.student.Applicant;
-import edu.sdsu.cs160l.university.student.Student;
-
 public interface UniversityApplication {
     void submitScore(Applicant s, float score);
-    void submitDocuments(Applicant s,String documents);
+
+    void submitDocuments(Applicant s, String documents);
+
     boolean checkStatus(Applicant applicant);
 }
