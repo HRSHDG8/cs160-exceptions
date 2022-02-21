e3deeac75b04f9b51f6aa0dad564c6cf4e54c5b3
HRSHDG8 <hrshdg8@gmail.com>
2022-02-21 14:32:02 -0800

    Singleton

--- src/main/java/edu/sdsu/cs160l/Main.java
+++ src/main/java/edu/sdsu/cs160l/Main.java
@@ -1,16 +1,9 @@
 package edu.sdsu.cs160l;
 
-import edu.sdsu.cs160l.university.course.CS340;
-import edu.sdsu.cs160l.university.registrar.Registrar;
-import edu.sdsu.cs160l.university.student.Student;
-import edu.sdsu.cs160l.university.student.StudentLevel;
-import edu.sdsu.cs160l.university.student.StudentMajor;
-import edu.sdsu.cs160l.university.student.TransferStudent;
-
 /**
  * Remember design principle 1.
  * Program to and interface and not an Implementation.
- *
+ * <p>
  * Design principle 2
  * Prefer a has a relationship over is a relationship.
  */
